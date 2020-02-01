5d2c7083dd2bf789ae813c52011dd789e9d03a67
Ihtml <2483690020@qq.com>
2020-02-01 14:07:42 +0800

    feat(b-1023): ac

---
+++ BasicLevel_C++/1023-组个最小数-20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  vector<int> v(10);
+  for (int i = 0; i < 10; i++) {
+    cin >> v[i];
+  }
+  for (int i = 1; i < 10; i++) {
+    if (v[i] != 0) {
+      cout << i;
+      v[i]--;
+      break;
+    }
+  }
+  for (int i = 0; i < 10; i++) {
+    if (v[i] != 0) {
+      for (int j = 0; j < v[i]; j++) {
+        cout << i;
+      }
+    }
+  }
+  return 0;
+}
