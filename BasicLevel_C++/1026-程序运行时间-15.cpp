44097324ab04f55aa068163768768ad273d435a0
Ihtml <2483690020@qq.com>
2020-02-04 01:34:15 +0800

    feat(b-1026): ac
    int四舍五入的方法，给被除数加上除数的一半；

---
+++ BasicLevel_C++/1026-程序运行时间-15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int c1, c2, t, h, m;
+    cin >> c1 >> c2;
+    t = (c2-c1+50)/100;
+    h = t/3600;
+    t = t%3600;
+    m = t/60;
+    t = t%60;
+    printf("%02d:%02d:%02d", h, m, t);
+}
