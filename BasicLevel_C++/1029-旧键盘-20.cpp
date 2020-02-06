8d470f8a1b71da3f638e74a059663c7b746ab632
Ihtml <2483690020@qq.com>
2020-02-06 13:12:39 +0800

    feat(b-1029): simplify code
    B.find(A[i]) == string::npos 判断字符串中是否存在某个字符，toupper()将字符转化为大写；

--- BasicLevel_C++/1029-旧键盘-20.cpp
+++ BasicLevel_C++/1029-旧键盘-20.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
-#include <map>
 using namespace std;
 int main() {
-  string A, B;
-  map<char, int> m;
+  string A, B, C;
   cin >> A >> B;
   for (int i = 0; i < A.length(); i++) {
-    if (islower(A[i])) {
-      A[i] -= 32;
-    }
-    m[A[i]] = 0;
-  }
-  for (int i = 0; i < B.length(); i++) {
-    if (islower(B[i])) {
-      B[i] -= 32;
-    }
-    m[B[i]] = 1;
-  }
-  for (int i = 0; i < A.length(); i++) {
-    if (m[A[i]] == 0) {
-      cout << A[i];
-      m[A[i]] = 1;
+    if (B.find(A[i]) == string::npos && C.find(toupper(A[i])) == string::npos) {
+      C += toupper(A[i]);
     }
   }
+  cout << C;
   return 0;
 }
