417fe794a3215b6cd99cf56c36a51cd095a2c2ad
Ihtml <2483690020@qq.com>
2020-02-08 14:27:14 +0800

    refactor(b-1031): optimize code
    使用函数优化代码结构

--- BasicLevel_C++/1031-查验身份证-15.cpp
+++ BasicLevel_C++/1031-查验身份证-15.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 using namespace std;
+int t[11] = {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+int q[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+string s;
+bool isTrue() {
+  int sum = 0;
+  for (int i = 0; i < 17; i++) {
+    if (s[i] < '0' || s[i] > '9') {
+      return false;
+    }
+    sum += q[i] * (s[i] - '0');
+  }
+  int temp = s[17] == 'X' ? 10 : s[17] - '0';
+  return t[sum%11] == temp;
+}
 int main() {
   int n;
-  string s;
   bool allpass = true;
-  char t[11] = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
-  int q[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
   cin >> n;
   for (int i = 0; i < n; i++) {
     cin >> s;
-    bool flag = true;
-    int sum = 0;
-    for (int j = 0; j < 17; j++) {
-      if (s[j] == 'X') {
-        cout << s << endl;
-        flag = false;
-        allpass = false;
-        break;
-      } else {
-        sum += (s[j] - '0') * q[j];
-      }
-    }
-    if (flag) {
-      sum = sum % 11;
-      if ((s[17]) != t[sum]) {
-        cout << s << endl;
-        allpass = false;
-      }
+    if (!isTrue()) {
+      cout << s << endl;
+      allpass = false;
     }
   }
   if (allpass) {
