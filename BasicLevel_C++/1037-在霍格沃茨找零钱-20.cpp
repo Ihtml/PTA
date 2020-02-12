0755a007666da81180b2cd0b1a791f910493c618
Ihtml <2483690020@qq.com>
2020-02-12 10:08:57 +0800

    feat(b-1037): ac
    使用scanf读取字符串中的整型数

--- BasicLevel_C++/1037-在霍格沃茨找零钱-20.cpp
+++ BasicLevel_C++/1037-在霍格沃茨找零钱-20.cpp
@@ -2,43 +2,17 @@
 #include <string>
 using namespace std;
 int main() {
-  string P, A;
-  cin >> P >> A;
   int Ps, Pg, Pk, As, Ag, Ak, idx1, idx2;
-  for (int i = 0; i < P.length(); i++) {
-    if (P[i] == '.') {
-      if (idx1) {
-        idx2 = i;
-      } else {
-        idx1 = i;
-      }
-    }
-  }
-  Pg = stoi(P.substr(0, idx1));
-  Ps = stoi(P.substr(idx1 + 1, idx2 - idx1 - 1));
-  Pk = stoi(P.substr(idx2 + 1));
-  idx1=0;
-  for (int i = 0; i < A.length(); i++) {
-    if (A[i] == '.') {
-      if (idx1) {
-        idx2 = i;
-      } else {
-        idx1 = i;
-      }
-    }
-  }
-  Ag = stoi(A.substr(0, idx1));
-  As = stoi(A.substr(idx1 + 1, idx2 - idx1 - 1));
-  Ak = stoi(A.substr(idx2 + 1));
+  scanf("%d.%d.%d %d.%d.%d", &Pg, &Ps, &Pk, &Ag, &As, &Ak);
   int res = (Ag * 17 * 29 + As * 29 + Ak) - (Pg * 17 * 29 + Ps * 29 + Pk);
   if (res < 0) {
-    cout << '-';
+    printf("-");
     res = abs(res);
   }
   int Rg = res / (17 * 29);
   int temp = res % (17 * 29);
   int Rs = temp / 29;
   int Rk = temp % 29;
-  cout << Rg << "." << Rs << "." << Rk;
+  printf("%d.%d.%d", Rg, Rs, Rk);
   return 0;
 }
