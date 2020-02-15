f1c3e1b0667ecd6214c50daf1ec454de98a2c1e0
Ihtml <2483690020@qq.com>
2020-02-15 20:20:09 +0800

    refactor(b-1041): simplify code
    使用二维数组，string arr[1001][2];

--- BasicLevel_C++/1041-考试座位号-15.cpp
+++ BasicLevel_C++/1041-考试座位号-15.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-struct student {
-  long long num;
-  int test;
-  int seat;
-};
-bool cmp(student a, student b) {
-  return a.test < b.test;
-}
 using namespace std;
 int main() {
-  int n, m, t, test, seat;
-  long long num;
-  student a;
+  int n, m, t;
+  string arr[1001][2], s1, s2;
   cin >> n;
-  vector<student> v(n);
   for (int i = 0; i < n; i++) {
-    scanf("%lld %d %d", &a.num, &a.test, &a.seat);
-    v[i] = a;
+    cin >> s1 >> t >> s2;
+    arr[t][0] = s1;
+    arr[t][1] = s2;
   }
-  sort(v.begin(), v.end(), cmp);
   cin >> m;
   for (int i = 0; i < m; i++) {
-      cin >> t;
-    if (i != 0) {
-      cout << endl;
-    }
-    cout << v[t-1].num << " " << v[t-1].seat;
+    cin >> t;
+    cout << arr[t][0] << " " << arr[t][1] << endl;
   }
   return 0;
 }
