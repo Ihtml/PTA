a8e958f897c34362c330021de2097b48b914359c
Ihtml <2483690020@qq.com>
2020-02-17 15:11:00 +0800

    refactor(b-1044): simplify code
    优化代码，同时遍历两个数组

--- BasicLevel_C++/1044-火星数字-20.cpp
+++ BasicLevel_C++/1044-火星数字-20.cpp
@@ -1,57 +1,41 @@
-#include <cctype>
 #include <iostream>
 #include <string>
 using namespace std;
 string low[13] = {"tret", "jan", "feb", "mar", "apr", "may", "jun",
                   "jly",  "aug", "sep", "oct", "nov", "dec"};
-string high[12] = {"tam", "hel", "maa", "huh", "tou", "kes",
-                   "hei", "elo", "syy", "lok", "mer", "jou"};
+string high[13] = {"****", "tam", "hel", "maa", "huh", "tou", "kes",
+                   "hei",  "elo", "syy", "lok", "mer", "jou"};
 int main() {
   int n, num;
-  string s, l, h;
   cin >> n;
   getchar();
   for (int i = 0; i < n; i++) {
+    string s, l="", h;
     getline(cin, s);
-    if (isdigit(s[0])) {
+    if ('0' <= s[0] && s[0] <= '9') {
       num = stoi(s);
-      if (num <= 13) {
+      if (num < 13) {
         printf("%s\n", low[num].c_str());
+      } else if (num%13 == 0) {
+        printf("%s\n", high[num / 13].c_str());
       } else {
-        printf("%s", high[num/13 -1].c_str());
-        if (num%13){
-            printf(" %s", low[num%13].c_str());
-        }
-        printf("\n");
+        printf("%s %s\n", high[num / 13].c_str(), low[num % 13].c_str());
       }
     } else {
-      if (s.find(" ") != string::npos) {
-        int idx = s.find(" ");
-        h = s.substr(0, idx);
-        l = s.substr(idx + 1);
-        int g, d;
-        for (int i = 0; i < 12; i++) {
-          if (high[i] == h) {
-            g = i+1;
-          }
+      h = s.substr(0, 3);
+      if (s.length() > 4) {
+        l = s.substr(4, 3);
+      }
+      int a = 0, b = 0;
+      for (int i = 1; i <= 12; i++) {
+        if (h == low[i] || l == low[i]) {
+          b = i;
         }
-        for (int i = 0; i < 13; i++) {
-          if (low[i] == l) {
-            d = i;
-          }
-        }
-        printf("%d\n", g * 13 + d);
-      } else {
-        for (int i = 0; i < 13; i++) {
-          if (low[i] == s){
-            printf("%d\n", i);
-            break;
-          }else if (high[i] == s){
-            printf("%d\n", (i+1)*13);
-            break;
-          }
+        if (h == high[i]) {
+          a = i;
         }
       }
+      printf("%d\n", a * 13 + b);
     }
   }
   return 0;
