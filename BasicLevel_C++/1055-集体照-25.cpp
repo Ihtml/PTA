9742172c1fc9c8aa63bbf8c884255fbc686d34c3
Ihtml <2483690020@qq.com>
2020-02-23 22:33:23 +0800

    feat(b-1055): ac

---
+++ BasicLevel_C++/1055-集体照-25.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+struct obj {
+  string name;
+  int high;
+};
+bool cmp(obj a, obj b) {
+  if (a.high > b.high) {
+    return true;
+  } else if (a.high == b.high && a.name < b.name) {
+    return true;
+  }
+  return false;
+}
+int main() {
+  int N, K, m;
+  cin >> N >> K;
+  vector<obj> v(N), temp;
+  vector<vector<obj> > res;
+  string name;
+  int high;
+  for (int i = 0; i < N; i++) {
+    cin >> name >> high;
+    v[i].name = name;
+    v[i].high = high;
+  }
+  sort(v.begin(), v.end(), cmp);
+  m = N / K;
+  int t, idx = 0;
+  for (int i = 0; i < K; i++) {
+    t = i == 0 ? N - m * (K - 1) : m;
+    vector<obj> temp(t);
+    int j;
+    for (int i = 0; i < t; i++) {
+      j = i % 2 == 0 ? t / 2 + 1 + i / 2 : t / 2 + 1 - (i + 1) / 2;
+      temp[j - 1] = v[idx];
+      idx++;
+    }
+    for (int i = 0; i < t; i++) {
+      if (i != 0) {
+        cout << " ";
+      }
+      cout << temp[i].name;
+    }
+    cout << endl;
+  }
+  return 0;
+}
