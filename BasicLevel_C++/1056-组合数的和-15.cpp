025c23f5a21bce17b5a38e94ea7fd628708cf389
Ihtml <2483690020@qq.com>
2020-02-24 10:13:39 +0800

    refactor(b-1056): optimize code

--- BasicLevel_C++/1056-组合数的和-15.cpp
+++ BasicLevel_C++/1056-组合数的和-15.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main() {
-  int N, sum = 0;
+  int N, sum = 0, temp;
   cin >> N;
-  vector<int> v(N);
   for (int i = 0; i < N; i++) {
-    cin >> v[i];
-  }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      sum += i == j ? 0 : v[i] * 10 + v[j];
-    }
+    cin >> temp;
+    sum += temp*10*(N-1) + temp*(N-1);
   }
   cout << sum;
   return 0;
