74bc259c5936ddfc38d273011170c53e9f0df4de
Ihtml <2483690020@qq.com>
2020-02-25 21:20:46 +0800

    refactor(b-1062): simplify code
    use swap()

--- BasicLevel_C++/1062-最简分数-20.cpp
+++ BasicLevel_C++/1062-最简分数-20.cpp
@@ -1,31 +1,27 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 using namespace std;
-int gcd(long long a, long long b){
-    return a==0 ? b : gcd(b%a, a);
+int gcd(long long a, long long b) {
+  return a == 0 ? b : gcd(b % a, a);
 }
-int main(){
-    int N1,N2,M1,M2,K, a, b, c;
-    scanf("%d/%d %d/%d %d", &N1, &M1 , &N2 , &M2, &K);
-    if(N1*M2 > N2*M1){
-        a = N2*K/M2 + 1;
-        b = M1;
-        c = N1;
-    }else{
-        a = N1*K/M1 +1;
-        b = M2;
-        c = N2;
+int main() {
+  int N1, N2, M1, M2, K, a;
+  scanf("%d/%d %d/%d %d", &N1, &M1, &N2, &M2, &K);
+  if (N1 * M2 > N2 * M1) {
+    swap(N1, N2);
+    swap(M1, M2);
+  }
+  a = N1 * K / M1 + 1;
+  int flag = 0;
+  while (a * M2 < N2 * K) {
+    if (gcd(a, K) == 1) {
+      if (flag) {
+        cout << " ";
+      }
+      cout << a << "/" << K;
+      flag = 1;
     }
-    int flag = 0;
-    while(a*b < c*K){
-        if(gcd(a,K) == 1){
-            if(flag){
-                cout << " ";
-            }
-            cout << a << "/" << K;
-            flag = 1;
-        }
-        a++;
-    }
-    return 0;
+    a++;
+  }
+  return 0;
 }
