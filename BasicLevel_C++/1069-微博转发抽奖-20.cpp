ff11d93dc1a05a6a8ef7bfc1c5d99cceb79c785c
Ihtml <2483690020@qq.com>
2020-02-28 03:28:36 +0800

    refactor(b-1069): simplify code
    边输入边判断，简化代码

--- BasicLevel_C++/1069-微博转发抽奖-20.cpp
+++ BasicLevel_C++/1069-微博转发抽奖-20.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <map>
-#include <vector>
 using namespace std;
 int main() {
   int m, n, s, ctn = 0;
   cin >> m >> n >> s;
-  vector<string> v(m);
   map<string, int> mp;
+  string name;
   for (int i = 0; i < m; i++) {
-    cin >> v[i];
-    mp[v[i]] = 0;
-  }
-  int t = -1;
-  for (int i = s - 1; i < m; i++) {
-    if (mp[v[i]] == 0) {
-      t++;
-    } else {
-      continue;
+    cin >> name;
+    if (mp[name] == 1) {
+      s = s + 1;
     }
-    if (t % (n) == 0) {
-      cout << v[i] << endl;
-      mp[v[i]]++;
+    if (mp[name] == 0 && i == s-1) {
+      mp[name] =1;
       ctn++;
+      cout << name << endl;
+      s += n;
     }
   }
   if (ctn == 0) {
