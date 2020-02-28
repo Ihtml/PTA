b2ade4270da59b508842061ea5917ac0d05c85ae
Ihtml <2483690020@qq.com>
2020-02-28 14:02:46 +0800

    refactor(b-1071): simplify code
    减少判断条件的代码

--- BasicLevel_C++/1071-小赌怡情-15.cpp
+++ BasicLevel_C++/1071-小赌怡情-15.cpp
@@ -5,23 +5,18 @@ int main() {
   cin >> T >> K;
   for (int i = 0; i < K; i++) {
     cin >> n1 >> b >> t >> n2;
-    if (t > T) {
-      printf("Not enough tokens.  Total = %d.\n", T);
-      continue;
-    }
-    int flag = 0;
-    if (b) {
-      T = n2 > n1 ? T + t : T - t;
-      flag = n2 > n1 ? 1 : 0;
-
-    } else {
-      T = n2 < n1 ? T + t : T - t;
-      flag = n2 < n1 ? 1 : 0;
-    }
-    printf("%s %d%c  Total = %d.\n", flag ? "Win" : "Lose", t, flag ? '!' : '.',T);
+    int flag = n2 > n1 ? 1 : 0;
     if (T == 0) {
       printf("Game Over.");
       break;
+    } else if (t > T) {
+      printf("Not enough tokens.  Total = %d.\n", T);
+    } else if (b == flag) {
+      T = T + t;
+      printf("Win %d!  Total = %d.\n", t, T);
+    } else {
+      T = T - t;
+      printf("Lose %d.  Total = %d.\n", t, T);
     }
   }
   return 0;
