7fd06960183ff39321d0be782d07639fa82eea69
Ihtml <2483690020@qq.com>
2020-03-06 15:08:52 +0800

    refactor(b-1077): simplify code

--- BasicLevel_C++/1077-互评成绩计算-20.cpp
+++ BasicLevel_C++/1077-互评成绩计算-20.cpp
@@ -1,27 +1,22 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
 int main() {
-  int n, m, t;
+  int n, m;
   cin >> n >> m;
   for (int i = 0; i < n; i++) {
-    vector<int> v;
-    int a, sum = 0;
-    cin >> a;
+    int g1, g2 = 0, maxg = -1, ming = m + 1, t, ctn = 0;
+    cin >> g1;
     for (int j = 0; j < n - 1; j++) {
       cin >> t;
-      if (t < 0 || t > m)
-        continue;
-      v.push_back(t);
+      if (0 <= t && t <= m) {
+        g2 += t;
+        ctn++;
+        if (t > maxg) maxg = t;
+        if (t < ming) ming = t;
+      }
     }
-    sort(v.begin(), v.end());
-    for (int k = 1; k < v.size() - 1; k++) {
-      sum += v[k];
-    }
-    sum = sum / (v.size() - 2);
-    sum = (sum + a + 1) / 2;
-    cout << sum << endl;
+    g2 = g2 - maxg - ming;
+    cout << int(((g2 * 1.0) / (ctn - 2) + g1 + 1) / 2) << endl;
   }
   return 0;
 }
