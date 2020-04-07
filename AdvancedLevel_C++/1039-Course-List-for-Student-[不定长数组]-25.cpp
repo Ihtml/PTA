c26b8217614719c717d5a6e0214172665b8c122b
Ihtml <2483690020@qq.com>
2020-04-07 20:00:01 +0800

    refactor(a-1039): optimize code

--- AdvancedLevel_C++/1039-Course-List-for-Student-[不定长数组]-25.cpp
+++ AdvancedLevel_C++/1039-Course-List-for-Student-[不定长数组]-25.cpp
@@ -1,34 +1,42 @@
 #include <algorithm>
 #include <cstdio>
-#include <string>
-#include <unordered_map>
 #include <vector>
 using namespace std;
+// 将3个英文和1个数字的字符串id, 转为int类型id
+int getid(char name[]) {
+  int id = 0;
+  for (int i = 0; i < 3; i++) {
+    id = id * 26 + name[i] - 'A';
+  }
+  id = id * 10 + name[3] - '0';
+  return id;
+}
+const int MAX_SIZE = 26 * 26 * 26 * 10 + 10;
 int main() {
   char name[5];
-  unordered_map<string, vector<int> > mp;
-  int n, k, id, m;
+  vector<int> arr[MAX_SIZE];
+  int n, k, id, m, index;
   scanf("%d %d", &n, &k);
   for (int i = 0; i < k; i++) {
     scanf("%d %d", &id, &m);
     for (int i = 0; i < m; i++) {
       scanf("%s", name);
-      mp[name].push_back(id);
+      index = getid(name);
+      arr[index].push_back(id);
     }
   }
   for (int i = 0; i < n; i++) {
     scanf("%s", name);
-    int len = mp[name].size();
+    index = getid(name);
+    int len = arr[index].size();
+    printf("%s %d", name, len);
     if (len) {
-      sort(mp[name].begin(), mp[name].end());
-      printf("%s %d", name, len);
-      for (int i = 0; i < len; i++) {
-        printf(" %d", mp[name][i]);
+      sort(arr[index].begin(), arr[index].end());
+      for (int j = 0; j < len; j++) {
+        printf(" %d", arr[index][j]);
       }
-      printf("\n");
-    } else {
-      printf("%s 0\n", name);
     }
+    printf("\n");
   }
   return 0;
 }
