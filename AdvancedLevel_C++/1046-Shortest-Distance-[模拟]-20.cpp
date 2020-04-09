e4b19f6934f695a9e1097337ca60b830b4ef63fc
Ihtml <2483690020@qq.com>
2020-04-09 20:53:36 +0800

    refactor(a-1046): optimize code
    使用swap和min函数, 同时使用累加法,提高效率

--- AdvancedLevel_C++/1046-Shortest-Distance-[模拟]-20.cpp
+++ AdvancedLevel_C++/1046-Shortest-Distance-[模拟]-20.cpp
@@ -2,26 +2,24 @@
 #include <vector>
 using namespace std;
 int main() {
-  int n, m, a, b, t, total = 0;
+  int n, m, a, b, total = 0;
   cin >> n;
-  vector<int> arr(n + 1);
+  vector<int> dis(n + 1);
   for (int i = 1; i <= n; i++) {
-    cin >> arr[i];
-    total += arr[i];
+    int t;
+    cin >> t;
+    total += t;
+    dis[i] = total;
   }
   cin >> m;
   for (int i = 0; i < m; i++) {
     int d = 0;
     cin >> a >> b;
     if (a > b) {
-      t = a;
-      a = b;
-      b = t;
+      swap(a, b);
     }
-    for (int i = a; i <= b - 1; i++) {
-      d += arr[i];
-    }
-    d = d < total - d ? d : total - d;
+    d = dis[b - 1] - dis[a - 1];
+    d = min(d, total - d);
     cout << d << endl;
   }
   return 0;
