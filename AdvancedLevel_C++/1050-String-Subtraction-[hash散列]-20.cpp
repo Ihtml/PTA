872f03008ed2fd459a1a7cb9566672bc3f2be78f
Ihtml <2483690020@qq.com>
2020-04-11 11:12:42 +0800

    refactor(a-1050): optimize code
    使用int hash[256] = {0};储存字符是否出现过;

--- AdvancedLevel_C++/1050-String-Subtraction-[hash散列]-20.cpp
+++ AdvancedLevel_C++/1050-String-Subtraction-[hash散列]-20.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 int main() {
   string s1, s2;
+  int hash[256] = {0};
   getline(cin, s1);
   getline(cin, s2);
-  for (int i = 0; i < s1.length(); i++) {
-    int flag = 0;
-    for (int j = 0; j < s2.length(); j++) {
-      if (s1[i] == s2[j]) {
-        flag = 1;
-        break;
-      }
-    }
-    if (!flag) {
+  int len1 = s1.length(), len2 = s2.length();
+  for (int i = 0; i < len2; i++) {
+    hash[s2[i]] = 1;
+  }
+  for (int i = 0; i < len1; i++) {
+    if (!hash[s1[i]]) {
       printf("%c", s1[i]);
     }
   }
