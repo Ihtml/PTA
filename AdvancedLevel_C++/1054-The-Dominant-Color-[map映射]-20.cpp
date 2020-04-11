ab63d188b8fc1e7ae0cb49cc99e3f30db58b3310
Ihtml <2483690020@qq.com>
2020-04-11 11:38:46 +0800

    refactor(a-1054): optimize code

--- AdvancedLevel_C++/1054-The-Dominant-Color-[map映射]-20.cpp
+++ AdvancedLevel_C++/1054-The-Dominant-Color-[map映射]-20.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 int main() {
   unordered_map<int, int> mp;
-  int m, n, d, max = 0;
+  int m, n, d;
   scanf("%d%d", &m, &n);
+  int half = m * n / 2;
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
       int t;
       scanf("%d", &t);
       mp[t]++;
-      if (mp[t] > max) {
-        d = t;
-        max = mp[t];
+      if (mp[t] > half) {
+        printf("%d", t);
+        return 0;
       }
     }
   }
-  printf("%d", d);
   return 0;
 }
